ee588716ff52271e0fc0d396ae3a7f9072e09fe2
dungdung05 <dungph50751@gmail.com>
2025-11-17 19:13:41 +0700

    Delete routes directory

--- routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const feedbackController = require("../controllers/feedbackController");
-
-router.get("/", feedbackController.getAllFeedbacks);
-router.post("/", feedbackController.createFeedback);
-
-module.exports = router;
