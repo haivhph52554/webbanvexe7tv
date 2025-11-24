1372663312d6daa59fd2cc724cc928a55499485b
Duong <duongnph50760@gmail.com>
2025-11-24 23:36:33 +0700

    ahihi

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -7,7 +7,7 @@ const User = require('../models/User');
 const Assistant = require('../models/Assistant');
 const TripSeatStatus = require('../models/TripSeatStatus');
 
-// --- Buses CRUD helpers for admin UI --- 12345678910
+// --- Buses CRUD helpers for admin UI --- 123456
 exports.newBus = async (req, res) => {
   try {
     res.render('admin/bus_form', { bus: null, page: 'buses', errors: null });
